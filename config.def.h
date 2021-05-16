8b40211bd96767b6d0b6eed021922c94973fd61b
dzwdz <kg67199@gmail.com>
2021-05-16 13:46:34 +0200

    Apply the border patch

--- config.def.h
+++ config.def.h
@@ -33,6 +33,9 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 
+/* Size of the window border */
+static const unsigned int border_width = 5;
+
 /*
  * Xresources preferences to load at startup
  */
